2d8e5fb3b8d346ba1675159b66658d711e0aa4d0
med aziz <mohamedaziz.cherif3741@istic.ucar.tn>
2024-03-22 00:30:00 +0100

    bug

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { WelcomeComponent } from './welcome/welcome.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, WelcomeComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'taste_nd_share';
