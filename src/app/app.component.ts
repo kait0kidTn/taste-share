74fb21d76a0cec8e7292c96c1d3ca0b2a70cf8e6
med aziz <mohamedaziz.cherif3741@istic.ucar.tn>
2024-03-22 01:12:55 +0100

    img

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { WelcomeComponent } from './welcome/welcome.component';
+import { ImageSliderComponent } from './image-slider/image-slider.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, WelcomeComponent],
+  imports: [RouterOutlet, WelcomeComponent, ImageSliderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
---
+++ src/app/image-slider/image-slider.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-image-slider',
+  standalone: true,
+  imports: [],
+  templateUrl: './image-slider.component.html',
+  styleUrl: './image-slider.component.css',
+})
+export class ImageSliderComponent implements OnInit {
+  images: string[] = [
+    'https://images.pexels.com/photos/1640777/pexels-photo-1640777.jpeg',
+    'https://images.pexels.com/photos/1279330/pexels-photo-1279330.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/808941/pexels-photo-808941.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  ];
+  currentIndex = 0;
+  currentImage!: string;
+  currentImageAlt!: string;
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.rotateImages();
+  }
+
+  rotateImages(): void {
+    setInterval(() => {
+      this.currentImage = this.images[this.currentIndex];
+      this.currentImageAlt = `Image ${this.currentIndex + 1}`;
+      this.currentIndex = (this.currentIndex + 1) % this.images.length;
+    }, 3000);
+  }
+}
