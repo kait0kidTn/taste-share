84030a9e177f86805cbded50233357103a1f83f4
med aziz <mohamedaziz.cherif3741@istic.ucar.tn>
2024-03-30 20:53:43 +0100

    edit without fix sign up

--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -11,16 +11,31 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './sign-up.component.css',
 })
 export class SignUpComponent {
+  passwordSignup: any;
+  confirmPasswordSignup: any;
   constructor(private readonly httpClient: HttpClient) {}
   public passwordType: string = 'password';
+  public confirmPasswordType: string = 'password';
+  public passwordMismatch: boolean = false;
   public changePasswordType(checked: boolean) {
     this.passwordType = checked ? 'text' : 'password';
   }
-  signup(username: string, email: string, password: string) {
-    console.log({ username, email, password });
+  public changeConfirmPasswordType(checked: boolean) {
+    this.confirmPasswordType = checked ? 'text' : 'password';
+  }
+  public changePasswordType1(checked: boolean) {
+    this.passwordType = checked ? 'text' : 'password';
+  }
+  public validatePasswords() {
+    const password = this.passwordSignup.value;
+    const confirmPassword = this.confirmPasswordSignup.value;
+    this.passwordMismatch = password !== confirmPassword;
+  }
+  signup(name: string, famname: string, email: string, password: string) {
+    console.log({ name, famname, email, password });
 
     this.httpClient
-      .post('http://localhost/signup.php', { username, email, password })
+      .post('http://localhost/signup.php', { name, email, password })
       .subscribe(
         (response) => {
           console.log('Signup successful:', response);
